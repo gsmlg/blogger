d4e0522b24f6ea1ebd9c756b8aea064fbd0d1f37
gsmlg <gsmlg.com@gmail.com>
2015-02-04 02:09:09 +0800

    add route

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('blogs', function(){
+    this.resource('blog', {path: ':blog_id'});
+  });
 });
 
 export default Router;
